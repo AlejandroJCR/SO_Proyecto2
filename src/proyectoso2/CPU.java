5f5cf259fed85f57523dd27b98fa4d74a46dcf42
Alejandro Contreras <a.contreras@correo.unimet.edu.ve>
2023-11-08 14:17:40 -0400

    Basic CPU & Character Classes

---
+++ src/proyectoso2/CPU.java
@@ -0,0 +1,48 @@
+package proyectoso2;
+
+public class CPU extends Thread{
+    
+    enum Result {WINNER, TIE, NULL}
+    
+    public CPU() {
+
+    }
+    
+    public void processWinner(){
+        // Choose winner based on its attributes
+        // Store winner in list of winners
+        // Erase loser from simulation
+    }
+    
+    public void processTie(){
+        // Put both caracters in their priority 1 queue IN THE LAST POSITION
+    }
+    
+    public void processNull(){
+        // Put both caracters in their reenforcement queue
+    }
+    
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                
+                // Wait for result
+                Thread.sleep(10*1000);
+                
+                // Process result
+                int random = (int)(Math.random() * 100);
+                if(random <= 40){
+                    processWinner();
+                } else if(random <= 67){
+                    processTie();
+                } else {
+                    processNull();
+                }     
+            }   
+        } catch(InterruptedException e){
+            //Thread killed
+        }
+        
+    }
+}
---
+++ src/proyectoso2/Character.java
@@ -0,0 +1,42 @@
+package proyectoso2;
+
+public class Character {
+    String PID;
+    int priority;
+    int counter;
+    
+    //Atributes
+    String name;
+    int qualityAttrs = 0;
+    int hitPoints, strength, agility;
+    
+    public Character(){
+        counter = 0;
+        getInitialPriority();
+    }
+    
+    public void checkQuality(int probability){
+        int random = (int)(Math.random() * 100);
+        if(random <= probability){
+            qualityAttrs++;
+        }   
+    }
+    
+    public void updateCounter(){
+        counter++;
+        if(counter == 8){
+            if(priority != 1){
+                counter = 0;
+                priority -= 1;
+            }
+        }
+    }
+    
+    public void getInitialPriority(){
+        checkQuality(60); //Abilities
+        checkQuality(70); // Hit Points
+        checkQuality(50); // Strenght
+        checkQuality(40); // Agility
+        
+    }
+}
